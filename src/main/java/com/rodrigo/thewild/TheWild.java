10cd4bc5657380fdfe379bd831dce7cd0485479b
Rodrigo66105 <rodsang78@gmail.com>
2026-09-16 17:39:00 +0200

    Fix animal spawn predicates and simplify mod event registration

--- src/main/java/com/rodrigo/thewild/TheWild.java
+++ src/main/java/com/rodrigo/thewild/TheWild.java
@@ -1,14 +1,13 @@
 package com.rodrigo.thewild;
 
 import com.rodrigo.thewild.registry.ModEntities;
-import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.SpawnPlacements;
+import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
 
 @Mod(TheWild.MOD_ID)
 public class TheWild {
@@ -17,7 +16,6 @@ public class TheWild {
     public TheWild() {
         var modBus = FMLJavaModLoadingContext.get().getModEventBus();
         ModEntities.ENTITIES.register(modBus);
-        modBus.register(ModEntities.class);
         modBus.addListener(this::commonSetup);
         modBus.addListener(this::createAttributes);
     }
@@ -25,13 +23,13 @@ public class TheWild {
     private void commonSetup(final FMLCommonSetupEvent event) {
         event.enqueueWork(() -> {
             SpawnPlacements.register(ModEntities.RED_DEER.get(), SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, Mob::checkMobSpawnRules);
+                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, Animal::checkAnimalSpawnRules);
             SpawnPlacements.register(ModEntities.WILD_BOAR.get(), SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, Mob::checkMobSpawnRules);
+                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, Animal::checkAnimalSpawnRules);
             SpawnPlacements.register(ModEntities.RED_FOX.get(), SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, Mob::checkMobSpawnRules);
+                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, Animal::checkAnimalSpawnRules);
             SpawnPlacements.register(ModEntities.GRAY_WOLF.get(), SpawnPlacements.Type.ON_GROUND,
-                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, Mob::checkMobSpawnRules);
+                    Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, Animal::checkAnimalSpawnRules);
         });
     }
 
@@ -40,9 +38,5 @@ public class TheWild {
         event.put(ModEntities.WILD_BOAR.get(), com.rodrigo.thewild.entity.WildBoarEntity.createAttributes().build());
         event.put(ModEntities.RED_FOX.get(), com.rodrigo.thewild.entity.RedFoxEntity.createAttributes().build());
         event.put(ModEntities.GRAY_WOLF.get(), com.rodrigo.thewild.entity.GrayWolfEntity.createAttributes().build());
-    }
-
-    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class ModEvents {
     }
 }
