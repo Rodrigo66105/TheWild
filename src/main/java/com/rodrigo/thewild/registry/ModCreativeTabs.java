5ef0ffe90565cfa1186fe09c6b52f30537fbf663
Rodrigo66105 <rodsang78@gmail.com>
2026-09-17 16:23:00 +0200

    Add TheWild creative inventory tab

---
+++ src/main/java/com/rodrigo/thewild/registry/ModCreativeTabs.java
@@ -0,0 +1,28 @@
+package com.rodrigo.thewild.registry;
+
+import com.rodrigo.thewild.TheWild;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public final class ModCreativeTabs {
+    public static final DeferredRegister<CreativeModeTab> TABS =
+            DeferredRegister.create(ForgeRegistries.CREATIVE_MODE_TABS, TheWild.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> THEWILD_TAB = TABS.register("thewild",
+            () -> CreativeModeTab.builder()
+                    .title(Component.translatable("itemGroup.thewild"))
+                    .icon(() -> new ItemStack(ModItems.RED_DEER_SPAWN_EGG.get()))
+                    .displayItems((parameters, output) -> {
+                        output.accept(ModItems.RED_DEER_SPAWN_EGG.get());
+                        output.accept(ModItems.WILD_BOAR_SPAWN_EGG.get());
+                        output.accept(ModItems.RED_FOX_SPAWN_EGG.get());
+                        output.accept(ModItems.GRAY_WOLF_SPAWN_EGG.get());
+                    })
+                    .build());
+
+    private ModCreativeTabs() {}
+}
