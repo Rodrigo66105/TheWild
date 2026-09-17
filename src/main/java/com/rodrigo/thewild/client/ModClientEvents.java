38b82d28b7f1107e363c16201924574c1dd06b00
Rodrigo66105 <rodsang78@gmail.com>
2026-09-17 17:25:46 +0200

    Register TheWild animal model layers

--- src/main/java/com/rodrigo/thewild/client/ModClientEvents.java
+++ src/main/java/com/rodrigo/thewild/client/ModClientEvents.java
@@ -1,6 +1,7 @@
 package com.rodrigo.thewild.client;
 
 import com.rodrigo.thewild.TheWild;
+import com.rodrigo.thewild.client.model.WildAnimalModel;
 import com.rodrigo.thewild.client.renderer.GrayWolfRenderer;
 import com.rodrigo.thewild.client.renderer.RedDeerRenderer;
 import com.rodrigo.thewild.client.renderer.RedFoxRenderer;
@@ -14,6 +15,18 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(modid = TheWild.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public final class ModClientEvents {
     private ModClientEvents() {}
+
+    @SubscribeEvent
+    public static void registerLayerDefinitions(EntityRenderersEvent.RegisterLayerDefinitions event) {
+        event.registerLayerDefinition(WildAnimalModel.DEER_LAYER,
+                () -> WildAnimalModel.createBodyLayer(WildAnimalModel.Type.DEER));
+        event.registerLayerDefinition(WildAnimalModel.FOX_LAYER,
+                () -> WildAnimalModel.createBodyLayer(WildAnimalModel.Type.FOX));
+        event.registerLayerDefinition(WildAnimalModel.WOLF_LAYER,
+                () -> WildAnimalModel.createBodyLayer(WildAnimalModel.Type.WOLF));
+        event.registerLayerDefinition(WildAnimalModel.BOAR_LAYER,
+                () -> WildAnimalModel.createBodyLayer(WildAnimalModel.Type.BOAR));
+    }
 
     @SubscribeEvent
     public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
